d4c9c72f2c2af19b33807100c23366db1d8a9a6f
sickee2 <sickee2@hotmail.com>
2025-11-28 19:10:33 +0800

    move main.cpp to test folder and rename to test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -1,9 +1,10 @@
-#include "gr/format.hh"
+#include <gr/format.hh>
 #include <gr/console.hh>
 #include <gr/performance_timer.hh>
 
 int main() {
   using namespace gr;
+  console::writeln("=== basic test ===");
   console::writeln("helle world {}", gr::toy::chrono::now());
   console::writeln("test .gitignore");
   console::writeln("new modify for git testing");
